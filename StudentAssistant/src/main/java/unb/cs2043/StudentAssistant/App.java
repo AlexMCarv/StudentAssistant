ee134cb9112c24cca999468e252a9844edb85986
AlexMCarv <alexandremcarv@gmail.com>
2018-10-11 17:07:38 -0300

    First Commit

---
+++ StudentAssistant/src/main/java/unb/cs2043/StudentAssistant/App.java
@@ -0,0 +1,13 @@
+package unb.cs2043.StudentAssistant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
