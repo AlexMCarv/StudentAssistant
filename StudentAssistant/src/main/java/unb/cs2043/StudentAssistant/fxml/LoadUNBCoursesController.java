c04b5843af1bb4fc2c7c66372ab88c882d102830
fredy20501 <fverret@unb.ca>
2018-11-03 19:33:28 -0300

    LoadUNBCourses now work, and uses mulltithreading when loading data.

--- StudentAssistant/src/main/java/unb/cs2043/StudentAssistant/fxml/LoadUNBCoursesController.java
+++ StudentAssistant/src/main/java/unb/cs2043/StudentAssistant/fxml/LoadUNBCoursesController.java
@@ -1,15 +1,24 @@
 package unb.cs2043.StudentAssistant.fxml;
 
 import unb.cs2043.StudentAssistant.Choice;
+import unb.cs2043.StudentAssistant.Schedule;
 import unb.cs2043.StudentAssistant.UNBCourseReader;
+
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.input.MouseEvent;
 
 /**
@@ -18,6 +27,10 @@ import javafx.scene.input.MouseEvent;
  */
 public class LoadUNBCoursesController implements javafx.fxml.Initializable {
 	
+	@FXML private ProgressIndicator loading;
+	@FXML private Label termLabel;
+	@FXML private Label levelLabel;
+    @FXML private Label locationLabel;
 	@FXML private ComboBox<Choice> termSelect;
     @FXML private ComboBox<Choice> levelSelect;
     @FXML private ComboBox<Choice> locationSelect;
@@ -26,8 +39,37 @@ public class LoadUNBCoursesController implements javafx.fxml.Initializable {
 	
     @Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-    	//Get choices
+    	setLoadingAnimation(true);
+    	
+    	//Initialize the selects on another thread
+    	SelectInitializer service = new SelectInitializer();
+        service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent t) {
+            	//Choose first choice as default
+            	termSelect.getSelectionModel().select(0);
+            	levelSelect.getSelectionModel().select(0);
+            	locationSelect.getSelectionModel().select(0);
+            	
+            	setLoadingAnimation(false);
+            }
+        });
+        service.start();
+	}
+    
+    private class SelectInitializer extends Service<Void> {
+        protected Task<Void> createTask() {
+            return new Task<Void>() {
+                protected Void call() {
+                	initializeSelects();
+                    return null;
+                }
+            };
+        }
+    }
+    
+	private void initializeSelects() {
+		//Get choices
     	Choice[][] choices = UNBCourseReader.getDropdownChoices();
     	ObservableList<Choice> termChoices = FXCollections.observableArrayList(choices[0]);
     	ObservableList<Choice> levelChoices = FXCollections.observableArrayList(choices[1]);
@@ -37,31 +79,79 @@ public class LoadUNBCoursesController implements javafx.fxml.Initializable {
     	termSelect.setItems(termChoices);
     	levelSelect.setItems(levelChoices);
     	locationSelect.setItems(locationChoices);
-    	
-    	//Choose first choice as default
-    	termSelect.getSelectionModel().select(0);
-    	levelSelect.getSelectionModel().select(0);
-    	locationSelect.getSelectionModel().select(0);
 	}
     
-    @FXML
-    private void closeWindow(MouseEvent event) {
-    	closeWindow();
-    }
+	private void setLoadingAnimation(boolean active) {
+		//Loading animation
+		loading.setVisible(active);
+		//Labels and selects
+		termLabel.setVisible(!active);
+		levelLabel.setVisible(!active);
+		locationLabel.setVisible(!active);
+		termSelect.setVisible(!active);
+		levelSelect.setVisible(!active);
+		locationSelect.setVisible(!active);
+		//Load button
+		loadBtn.setDisable(active);
+	}
 
     @FXML
     private void loadData(MouseEvent event) {
+    	setLoadingAnimation(true);
+    	
     	//Get selected choice
     	String termVal = termSelect.getSelectionModel().getSelectedItem().getValue();
     	String levelVal = levelSelect.getSelectionModel().getSelectedItem().getValue();
     	String locationVal = locationSelect.getSelectionModel().getSelectedItem().getValue();
     	
-    	System.out.println("term: "+termVal+"\nlevel: "+levelVal+"\nlocation: "+locationVal);
+    	//Load the data on another thread
+    	DataLoader service = new DataLoader();
+    	service.setTerm(termVal);
+    	service.setLevel(levelVal);
+    	service.setLocation(locationVal);
+        service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent t) {
+            	setLoadingAnimation(false);
+            	
+            	File file = (File) t.getSource().getValue();
+				Schedule courseList = UNBCourseReader.readFile(file.getName());
+				
+				//todo: Send courseList to main controller
+				System.out.println(courseList);
+				
+				//closeWindow();
+            }
+        });
+        service.start();
     	
-    	//Send values to main controller
-    	
-    	//Close window
-//    	closeWindow();
+       
+    }
+    
+    private static class DataLoader extends Service<File> {
+    	private String term;
+    	private String level;
+    	private String location;
+        public void setTerm(String term) {this.term = term;}
+		public void setLevel(String level) {this.level = level;}
+		public void setLocation(String location) {this.location = location;}
+		protected Task<File> createTask() {
+            return new Task<File>() {
+                protected File call() {
+                    UNBCourseReader reader = new UNBCourseReader(term, level, location);
+                    File file = reader.getFile();
+                    if (!file.exists()) {
+                    	reader.loadData();
+                    }
+                    return file;
+                }
+            };
+        }
+    }
+    
+    @FXML
+    private void closeWindow(MouseEvent event) {
+    	closeWindow();
     }
     
     private void closeWindow() {
