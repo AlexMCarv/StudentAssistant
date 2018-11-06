5586a30f490bb553a11421d570e9d07f6a7e1def
Alexandre Carvalho <alexandremcarv@gmail.com>
2018-11-06 11:59:34 -0400

    Items in treeview now display names by following the hierarchy

--- StudentAssistant/src/main/java/unb/cs2043/student_assistant/fxml/TreeViewGenericCell.java
+++ StudentAssistant/src/main/java/unb/cs2043/student_assistant/fxml/TreeViewGenericCell.java
@@ -1,7 +1,7 @@
 package unb.cs2043.student_assistant.fxml;
 
 import javafx.scene.control.TreeCell;
-import unb.cs2043.student_assistant.Course;
+import unb.cs2043.student_assistant.ClassTime;
 
 /**
  * Controls how the objects are shown when added to a TreeView item 
@@ -18,6 +18,11 @@ public class TreeViewGenericCell <T> extends TreeCell<T>{
             
 		} else {
 			setText(item.toString());
+			if (!(this.getItem() instanceof ClassTime)) {
+				int colonPos = item.toString().indexOf(':');
+				String text = item.toString().substring(0, colonPos);
+				setText(text);
+			}
 		}
 	}
 }
