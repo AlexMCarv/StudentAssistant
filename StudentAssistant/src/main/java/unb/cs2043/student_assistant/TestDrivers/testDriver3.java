bbcf8a0bfde456c820df136f31d801e35075bd00
grantaid <shuttyt@gmail.com>
2018-11-08 07:26:09 -0400

    testing

---
+++ StudentAssistant/src/main/java/unb/cs2043/student_assistant/TestDrivers/testDriver3.java
@@ -0,0 +1,31 @@
+package unb.cs2043.student_assistant.TestDrivers;
+
+import unb.cs2043.student_assistant.ClassTime;
+import unb.cs2043.student_assistant.Course;
+import unb.cs2043.student_assistant.Schedule;
+import unb.cs2043.student_assistant.Section;
+
+import java.util.ArrayList;
+/**@author Tye Shutty*/
+public class testDriver3{
+	public static void main(String[] args){
+		Schedule one=new Schedule("Jane's Winter 2019");
+		ArrayList<Course> two=one.copyList();
+		Course temp;
+		Section temp1;
+		ClassTime temp2;
+		for(int x=0; x< 10; x++){
+			temp2=new ClassTime("Time"+x*x*x);
+			temp1=new Section("Section"+x*x);
+			temp1.add(temp2);
+			temp=new Course("Course"+x);
+			temp.add(temp1);
+			one.add(temp);
+
+		}
+		System.out.println(one);
+		if(two.getClassTime(0)==null){
+			System.out.println("2 is empty");
+		}
+	}
+}
