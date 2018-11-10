a1f4dfd2f5c582cd7dcca53ece7c59b010fc9099
Alexandre Carvalho <alexandremcarv@gmail.com>
2018-11-10 17:15:04 -0400

    Created unit tests for ClassTime class

--- StudentAssistant/src/test/java/unb/cs2043/StudentAssistant/ClassTimeTest.java
+++ StudentAssistant/src/test/java/unb/cs2043/StudentAssistant/ClassTimeTest.java
@@ -2,58 +2,118 @@ package unb.cs2043.StudentAssistant;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
 import org.junit.Test;
+import unb.cs2043.student_assistant.ClassTime;
 
 public class ClassTimeTest {
 
 	@Test
-	public void testClassTime() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testGetDays() {
-		fail("Not yet implemented");
+		LocalTime start = LocalTime.of(21, 30);
+		LocalTime end = LocalTime.of(22, 30);
+		ArrayList<String> days = new ArrayList<>();
+		days.add("Su");
+		days.add("M");
+		days.add("Th");
+		ClassTime time = new ClassTime("Lec", days, start, end);
+		assertEquals("SuMTh", time.getDays());
+		
+		days.remove("M");
+		assertEquals("SuTh", time.getDays());
 	}
 
 	@Test
 	public void testGetStartTime() {
-		fail("Not yet implemented");
+		LocalTime start = LocalTime.of(21, 30);
+		LocalTime end = LocalTime.of(22, 30);
+		ArrayList<String> days = new ArrayList<>();
+		days.add("Su");
+		ClassTime time = new ClassTime("Lec", days, start, end);
+		
+		assertEquals(LocalTime.of(21, 30), time.getStartTime());
+		assertEquals("21:30", time.getStartTime().toString());
 	}
 
 	@Test
 	public void testGetEndTime() {
-		fail("Not yet implemented");
+		LocalTime start = LocalTime.of(21, 30);
+		LocalTime end = LocalTime.of(22, 30);
+		ArrayList<String> days = new ArrayList<>();
+		days.add("Su");
+		ClassTime time = new ClassTime("Lec", days, start, end);
+		
+		assertEquals(LocalTime.of(22, 30), time.getEndTime());
+		assertEquals("22:30", time.getEndTime().toString());
 	}
 
 	@Test
 	public void testGetType() {
-		fail("Not yet implemented");
+		LocalTime start = LocalTime.of(21, 30);
+		LocalTime end = LocalTime.of(22, 30);
+		ArrayList<String> days = new ArrayList<>();
+		days.add("Su");
+		ClassTime time = new ClassTime("Lec", days, start, end);
+				
+		assertEquals("Lec", time.getType());
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		LocalTime start = LocalTime.of(21, 30);
+		LocalTime end = LocalTime.of(22, 30);
+		ArrayList<String> days = new ArrayList<>();
+		days.add("Su");
+		days.add("M");
+		days.add("Th");
+		ClassTime time = new ClassTime("Lec", days, start, end);
+				
+		assertEquals("Lec SuMTh 21:30-22:30", time.toString());
 	}
 
 	@Test
 	public void testSetDays() {
-		fail("Not yet implemented");
+		LocalTime start = LocalTime.of(21, 30);
+		LocalTime end = LocalTime.of(22, 30);
+		ArrayList<String> days = new ArrayList<>();
+		days.add("Su");
+		days.add("M");
+		ClassTime time = new ClassTime("Lec", days, start, end);
+		
+		ArrayList<String> newDays = new ArrayList<>();
+		newDays.add("T");
+		newDays.add("Th");
+		time.setDays(newDays);
+		
+		assertEquals("TTh", time.getDays());
 	}
 
 	@Test
 	public void testSetStartTime() {
-		fail("Not yet implemented");
+		LocalTime start = LocalTime.of(21, 30);
+		LocalTime end = LocalTime.of(22, 30);
+		ArrayList<String> days = new ArrayList<>();
+		days.add("Su");
+		ClassTime time = new ClassTime("Lec", days, start, end);
+		
+		LocalTime newTime = LocalTime.of(21, 45);
+		time.setStartTime(newTime);
+		
+		assertEquals("21:45", time.getStartTime().toString());
 	}
 
 	@Test
 	public void testSetEndTime() {
-		fail("Not yet implemented");
+		LocalTime start = LocalTime.of(21, 30);
+		LocalTime end = LocalTime.of(22, 30);
+		ArrayList<String> days = new ArrayList<>();
+		days.add("Su");
+		ClassTime time = new ClassTime("Lec", days, start, end);
+		
+		LocalTime newTime = LocalTime.of(22, 55);
+		time.setEndTime(newTime);
+		
+		assertEquals("22:55", time.getEndTime().toString());
 	}
-
-	@Test
-	public void testIsValidDay() {
-		fail("Not yet implemented");
-	}
-
 }
