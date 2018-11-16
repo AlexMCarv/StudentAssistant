30c2a884df468365764c18df1056b69f21844a60
fredy20501 <fverret@unb.ca>
2018-11-16 19:16:18 -0400

    Created junit test for equals() method of Schedule.

--- StudentAssistant/src/main/java/unb/cs2043/student_assistant/ScheduleArranger.java
+++ StudentAssistant/src/main/java/unb/cs2043/student_assistant/ScheduleArranger.java
@@ -1,5 +1,7 @@
 package unb.cs2043.student_assistant;
 
+import java.util.TreeSet;
+
 /**
  * This class contains the main algorithm of this application 
  * which finds the best schedule arrangement(s) given a set of courses.
@@ -7,7 +9,34 @@ package unb.cs2043.student_assistant;
  */
 public class ScheduleArranger {
 	
+	//The array returned contains (at most) 4 schedule objects
 	public static Schedule[] getBestSchedules(Schedule courseList) {
+		//Use a set to prevent duplicates
+		TreeSet<Schedule> scheduleArrangements = new TreeSet<>();
+		
+		int numCourses = courseList.getSize();
+		
+		//Each course has an index (indicating what section to add)
+		int[] indexes = new int[numCourses];
+		
+		//Initialize all indexes to 0
+		for (int i=0; i<numCourses; i++) {
+			indexes[i] = 0;
+		}
+		
+		boolean done = false;
+		int numSchedules = 0;
+		while (!done) {
+			Schedule currentSchedule = new Schedule("S"+numSchedules);
+			
+			for (int i=0; i<numCourses && !done; i++) {
+				
+			}
+			
+			numSchedules++;
+		}
+		
+		
 		return null;
 	}
 }
