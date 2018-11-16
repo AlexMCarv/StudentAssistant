36b46ad6b15da09c2b580f894a40974a9db67d80
fredy20501 <fverret@unb.ca>
2018-11-15 20:06:34 -0400

    Added ScheduleArranger class that will hold the main algorithm.

---
+++ StudentAssistant/src/main/java/unb/cs2043/student_assistant/ScheduleArranger.java
@@ -0,0 +1,13 @@
+package unb.cs2043.student_assistant;
+
+/**
+ * This class contains the main algorithm of this application 
+ * which finds the best schedule arrangement(s) given a set of courses.
+ * @author Frederic Verret
+ */
+public class ScheduleArranger {
+	
+	public static Schedule[] getBestSchedules(Schedule courseList) {
+		return null;
+	}
+}
