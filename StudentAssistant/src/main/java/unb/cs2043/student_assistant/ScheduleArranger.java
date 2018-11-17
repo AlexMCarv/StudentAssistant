37c0c1f500b34414dadc6fde77802a2837a29021
fredy20501 <fverret@unb.ca>
2018-11-16 20:21:34 -0400

    Progress with algorithm (don't bother looking at it now, it is a mess)
    **Not even close to working yet, just had to save my progress because of
    how eclipse/git works.

--- StudentAssistant/src/main/java/unb/cs2043/student_assistant/ScheduleArranger.java
+++ StudentAssistant/src/main/java/unb/cs2043/student_assistant/ScheduleArranger.java
@@ -11,32 +11,68 @@ public class ScheduleArranger {
 	
 	//The array returned contains (at most) 4 schedule objects
 	public static Schedule[] getBestSchedules(Schedule courseList) {
-		//Use a set to prevent duplicates
+		//Use a set to prevent duplicates in results
 		TreeSet<Schedule> scheduleArrangements = new TreeSet<>();
 		
 		int numCourses = courseList.getSize();
 		
 		//Each course has an index (indicating what section to add)
 		int[] indexes = new int[numCourses];
-		
-		//Initialize all indexes to 0
-		for (int i=0; i<numCourses; i++) {
+		//Stores the max number for each index (how many sections in each course)
+		int[] maxIndexes = new int[numCourses];
+		//Initialize all indexes to 0, and set max indexes
+		for (int i=0; i<indexes.length; i++) {
 			indexes[i] = 0;
+			maxIndexes[i] = courseList.getCourse(i).getSize();
 		}
 		
-		boolean done = false;
 		int numSchedules = 0;
+		boolean done = false;
 		while (!done) {
 			Schedule currentSchedule = new Schedule("S"+numSchedules);
 			
 			for (int i=0; i<numCourses && !done; i++) {
+				Course currentCourse = courseList.getCourse(i);
+				Section sectionToAdd = currentCourse.getSection(indexes[i]);
+				
+				if (noConflictsBetween(currentSchedule, sectionToAdd)) {
+					currentSchedule.add(currentCourse);
+				}
 				
 			}
 			
+			if (!incrementAsCounter(indexes, maxIndexes)) {
+				done = true;
+			}
+			
+			scheduleArrangements.add(currentSchedule);
 			numSchedules++;
 		}
 		
 		
 		return null;
+	}
+	
+	
+	//Return false if any section in schedule conflicts with section, true otherwise.
+	private static boolean noConflictsBetween(Schedule schedule, Section section) {
+		boolean noConflicts = true;
+		
+		for (int i=0; i<schedule.copyCourses().size() && noConflicts; i++) {
+			Course currentCourse = schedule.getCourse(i);
+			for (int j=0; j<currentCourse.copySections().size() && noConflicts; j++) {
+				Section currentSection = currentCourse.getSection(j);
+				noConflicts = !currentSection.conflictsWith(section);
+			}
+		}
+		
+		return noConflicts;
+	}
+	
+	
+	//Increments an array of integers in a fashion similar to a counter.
+	private static boolean incrementAsCounter(int[] indexes, int[] maxIndexes) {
+		//TODO
+		return false;
 	}
 }
