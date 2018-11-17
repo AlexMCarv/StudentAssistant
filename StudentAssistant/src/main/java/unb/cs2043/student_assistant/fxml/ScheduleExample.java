3a98d8d80179febe205fcda12ee63f41133fc5e6
Alexandre Carvalho <alexandremcarv@gmail.com>
2018-11-17 15:21:56 -0400

    Added Sample code with standard spreadsheet. Mod size to match schedule

--- StudentAssistant/src/main/java/unb/cs2043/student_assistant/fxml/ScheduleExample.java
+++ StudentAssistant/src/main/java/unb/cs2043/student_assistant/fxml/ScheduleExample.java
@@ -2,14 +2,20 @@ package unb.cs2043.student_assistant.fxml;
 
 import java.util.ServiceLoader;
 
+import org.controlsfx.control.spreadsheet.GridBase;
+import org.controlsfx.control.spreadsheet.SpreadsheetCell;
+import org.controlsfx.control.spreadsheet.SpreadsheetCellType;
 import org.controlsfx.control.spreadsheet.SpreadsheetView;
 
 import fxsampler.FXSamplerConfiguration;
 import fxsampler.SampleBase;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -17,8 +23,9 @@ import unb.cs2043.student_assistant.fxml.HelloSpreadsheetView.SpreadsheetViewExa
 
 public class ScheduleExample extends Application {
 	
-	private SpreadsheetView spreadSheetView;
-	private StackPane centerPane;
+	private SpreadsheetView spreadSheetView1;
+	private SpreadsheetView spreadSheetView2;
+	private HBox centerPane;
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -26,7 +33,8 @@ public class ScheduleExample extends Application {
 
         primaryStage.setTitle("Schedule Example");
         VBox root = new VBox();
-        Scene scene = new Scene((Parent) getPanel(primaryStage), 800, 800);
+        root.getChildren().addAll(getPanel(primaryStage));
+        Scene scene = new Scene(root, 800, 800);
         scene.getStylesheets().add(SampleBase.class.getResource("fxsampler.css").toExternalForm());
         for (FXSamplerConfiguration fxsamplerConfiguration : configurationServiceLoader) {
         	String stylesheet = fxsamplerConfiguration.getSceneStylesheet();
@@ -43,8 +51,24 @@ public class ScheduleExample extends Application {
 	}
 	
 	public Node getPanel(Stage stage) {
-        spreadSheetView = new ScheduleDisplay();
-        centerPane = new StackPane(spreadSheetView);
+        spreadSheetView1 = new ScheduleDisplay();
+        int rowCount = 30;
+        int columnCount = 8;
+        GridBase grid = new GridBase(rowCount, columnCount);
+        ObservableList<ObservableList<SpreadsheetCell>> rows = FXCollections.observableArrayList();
+        for (int row = 0; row < grid.getRowCount(); ++row) {
+            final ObservableList<SpreadsheetCell> list = FXCollections.observableArrayList();
+            for (int column = 0; column < grid.getColumnCount(); ++column) {
+                list.add(SpreadsheetCellType.STRING.createCell(row, column, 1, 1,"value"));
+            }
+            rows.add(list);
+        }
+        grid.setRows(rows);
+        
+        spreadSheetView2 = new SpreadsheetView(grid);
+        spreadSheetView2.setShowRowHeader(true);
+        spreadSheetView2.setShowColumnHeader(true);
+        centerPane = new HBox(spreadSheetView1, spreadSheetView2);
         return centerPane;
     }
 }
