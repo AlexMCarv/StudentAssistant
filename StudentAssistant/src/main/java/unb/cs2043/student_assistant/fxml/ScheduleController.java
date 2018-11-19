be156867897f4bef87facf120698208ad1a2f6fc
Alexandre Carvalho <alexandremcarv@gmail.com>
2018-11-19 18:23:21 -0400

    Added functionality in the buttons to change the schedule to display

--- StudentAssistant/src/main/java/unb/cs2043/student_assistant/fxml/ScheduleController.java
+++ StudentAssistant/src/main/java/unb/cs2043/student_assistant/fxml/ScheduleController.java
@@ -8,6 +8,7 @@ import org.controlsfx.glyphfont.Glyph;
 import org.controlsfx.glyphfont.GlyphFontRegistry;
 import org.controlsfx.samples.HelloGlyphFont;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
@@ -32,6 +33,7 @@ public class ScheduleController implements javafx.fxml.Initializable {
 			displayList[i] = new ScheduleDisplay(bestSchedules[i]);
 			anchorPane.getChildren().add(displayList[i]);
 			buttonList[i] = new Button("Schedule " + (i+1), new Glyph("FontAwesome", FontAwesome.Glyph.CALENDAR));
+			buttonList[i].setOnAction(this::selectSchedule);
 			toolbar.getItems().add(buttonList[i]);
 		}
 		
@@ -39,5 +41,15 @@ public class ScheduleController implements javafx.fxml.Initializable {
 
 	public void setBestSchedules(Schedule[] list) {
 		bestSchedules = list;
+	}
+	
+	public void selectSchedule(ActionEvent e) {
+		
+		Button source = (Button) e.getSource();
+		for(int i = 0; i < buttonList.length; i++) {
+			displayList[i].setVisible(false);
+			if (source.equals(buttonList[i]))
+				displayList[i].setVisible(true);
+		}
 	}
 }
